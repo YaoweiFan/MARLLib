ede3b40c62c9d9f8045e8bb8401af87aff0860fd
YaoweiFan <3313668024@qq.com>
2022-11-02 14:45:19 +0800

    add dual_arm_rod_env

--- envs/__init__.py
+++ envs/__init__.py
@@ -2,10 +2,12 @@ from functools import partial
 
 from .multi_agent_env import MultiAgentEnv
 from .dual_arm_env import DualArmEnv
+from .dual_arm_rod_env import DualArmRodEnv
 
 
 def env_fn(env, **kwargs) -> MultiAgentEnv:
     return env(**kwargs)
 
 
-ENV = {"dualarm": partial(env_fn, env=DualArmEnv)}
+ENV = {"dualarm": partial(env_fn, env=DualArmEnv),
+       "dualarmrod": partial(env_fn, env=DualArmRodEnv)}
