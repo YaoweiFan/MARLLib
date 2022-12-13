0169fdf6b180aaa2f316d1c3a1b15f9e018d99eb
YaoweiFan <3313668024@qq.com>
2022-12-13 14:10:44 +0800

    soft adjust

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,7 +1,6 @@
 from functools import partial
 
 from .multi_agent_env import MultiAgentEnv
-from .dual_arm_env import DualArmEnv
 from .dual_arm_env_continuous import DualArmContinuousEnv
 from .dual_arm_rod_env_continuous import DualArmRodContinuousEnv
 
@@ -10,6 +9,5 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
     return env(**kwargs)
 
 
-ENV = {"dualarm": partial(env_fn, env=DualArmEnv),
-       "dualarmcontinuous": partial(env_fn, env=DualArmContinuousEnv),
+ENV = {"dualarmcontinuous": partial(env_fn, env=DualArmContinuousEnv),
        "dualarmrodcontinuous": partial(env_fn, env=DualArmRodContinuousEnv)}
